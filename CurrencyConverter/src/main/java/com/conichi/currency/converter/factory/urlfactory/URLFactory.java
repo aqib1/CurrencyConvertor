17bbe849464c159533f1022499530b2d2bd30676
Aqib Javed <aqibbutt3078@gmail.com>
2019-09-18 14:03:39 +0500

    Updated, Service and gitignore file

---
+++ CurrencyConverter/src/main/java/com/conichi/currency/converter/factory/urlfactory/URLFactory.java
@@ -0,0 +1,22 @@
+package com.conichi.currency.converter.factory.urlfactory;
+
+import com.conichi.currency.converter.constant.URLS;
+import com.example.model.URLTypes;
+
+public class URLFactory {
+
+	public String getURL(URLTypes urlType) {
+		String url = URLS.CURRENCYLAYER_API;
+		switch (urlType) {
+		case CONVERT:
+			url += URLS.CURRENCYLAYER_API_CONVERT;
+			break;
+
+		default:
+		}
+
+		url += "?" + URLS.ACCESS_KEY + "=" + URLS.ACCESS_KEY_VALUE;
+		return url;
+
+	}
+}
---
+++ CurrencyConverter/src/main/java/com/conichi/currency/converter/service/CurrencyConverterService.java
@@ -0,0 +1,6 @@
+package com.conichi.currency.converter.service;
+
+public interface CurrencyConverterService {
+
+	
+}
