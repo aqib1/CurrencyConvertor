1dfc7cfc0431baef74010d00ed51b12495e4bea9
Aqib Javed <aqibbutt3078@gmail.com>
2019-09-24 10:22:39 +0500

    ResponseError added

--- CurrencyConverter/src/main/java/com/conichi/currency/converter/controller/advices/CurrencyConverterAdvice.java
+++ CurrencyConverter/src/main/java/com/conichi/currency/converter/controller/advices/CurrencyConverterAdvice.java
@@ -1,5 +1,6 @@
 package com.conichi.currency.converter.controller.advices;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -16,6 +17,7 @@ import com.conichi.currency.converter.exceptions.CacheRefresherException;
 import com.conichi.currency.converter.exceptions.InvalidParamException;
 import com.conichi.currency.converter.exceptions.InvalidRequestException;
 import com.conichi.currency.converter.exceptions.InvalidResponseException;
+import com.example.model.ResponseError;
 
 @RestControllerAdvice
 public class CurrencyConverterAdvice {
@@ -23,50 +25,68 @@ public class CurrencyConverterAdvice {
 	private static final Logger logger = LoggerFactory.getLogger(CurrencyConverterAdvice.class);
 
 	@ExceptionHandler(value = { BadInternalServerException.class })
-	public ResponseEntity<String> handleBadInternalServerException(RuntimeException e, WebRequest wr) {
+	public ResponseEntity<ResponseError> handleBadInternalServerException(RuntimeException e, WebRequest wr) {
 		String error = Optional.of(e.getMessage()).orElse(e.getClass().getName())
-				+ "\n[Bad internal server exception! => (BadInternalServerException)]";
-		logger.error(error, e);
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+				+ " [Bad internal server exception! => (BadInternalServerException)]";
+		ResponseError errorResponse = new ResponseError().createdAt(LocalDateTime.now().toString())
+				.detailedMessage(error).errorCode(HttpStatus.BAD_REQUEST.value())
+				.exceptionName(BadInternalServerException.class.getName()).errorMessage(e.getMessage());
+		logger.error(errorResponse.toString(), e);
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(value = { InvalidParamException.class })
-	public ResponseEntity<String> handleInvalidParamException(RuntimeException e, WebRequest wr) {
+	public ResponseEntity<ResponseError> handleInvalidParamException(RuntimeException e, WebRequest wr) {
 		String error = Optional.of(e.getMessage()).orElse(e.getClass().getName())
-				+ "\n[Invalid Params! => (InvalidParamException)]";
-		logger.error(error, e);
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+				+ " [Invalid Params! => (InvalidParamException)]";
+		ResponseError errorResponse = new ResponseError().createdAt(LocalDateTime.now().toString())
+				.detailedMessage(error).errorCode(HttpStatus.BAD_REQUEST.value())
+				.exceptionName(InvalidParamException.class.getName()).errorMessage(e.getMessage());
+		logger.error(errorResponse.toString(), e);
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(value = { InvalidRequestException.class })
-	public ResponseEntity<String> handleInvalidRequestException(RuntimeException e, WebRequest wr) {
+	public ResponseEntity<ResponseError> handleInvalidRequestException(RuntimeException e, WebRequest wr) {
 		String error = Optional.of(e.getMessage()).orElse(e.getClass().getName())
-				+ "\n[Invalid Request! => (InvalidRequestException)]";
-		logger.error(error, e);
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+				+ " [Invalid Request! => (InvalidRequestException)]";
+		ResponseError errorResponse = new ResponseError().createdAt(LocalDateTime.now().toString())
+				.detailedMessage(error).errorCode(HttpStatus.BAD_REQUEST.value())
+				.exceptionName(InvalidRequestException.class.getName()).errorMessage(e.getMessage());
+		logger.error(errorResponse.toString(), e);
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(value = { InvalidResponseException.class })
-	public ResponseEntity<String> handleInvalidResponseException(RuntimeException e, WebRequest wr) {
+	public ResponseEntity<ResponseError> handleInvalidResponseException(RuntimeException e, WebRequest wr) {
 		String error = Optional.of(e.getMessage()).orElse(e.getClass().getName())
-				+ "\n[Invalid Response! => (InvalidResponseException)]";
-		logger.error(error, e);
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+				+ " [Invalid Response! => (InvalidResponseException)]";
+		ResponseError errorResponse = new ResponseError().createdAt(LocalDateTime.now().toString())
+				.detailedMessage(error).errorCode(HttpStatus.BAD_REQUEST.value())
+				.exceptionName(InvalidResponseException.class.getName()).errorMessage(e.getMessage());
+		logger.error(errorResponse.toString(), e);
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(value = { CachePresistException.class })
-	public ResponseEntity<String> handleCachePresistException(RuntimeException e, WebRequest wr) {
+	public ResponseEntity<ResponseError> handleCachePresistException(RuntimeException e, WebRequest wr) {
 		String error = Optional.of(e.getMessage()).orElse(e.getClass().getName())
-				+ "\n[Cache presistence failed! => (CachePresistException)]";
-		logger.error(error, e);
-		return new ResponseEntity<>(error, HttpStatus.EXPECTATION_FAILED);
+				+ " [Cache presistence failed! => (CachePresistException)]";
+		ResponseError errorResponse = new ResponseError().createdAt(LocalDateTime.now().toString())
+				.detailedMessage(error).errorCode(HttpStatus.EXPECTATION_FAILED.value())
+				.exceptionName(CachePresistException.class.getName()).errorMessage(e.getMessage());
+		logger.error(errorResponse.toString(), e);
+		return new ResponseEntity<>(errorResponse, HttpStatus.EXPECTATION_FAILED);
 	}
 
 	@ExceptionHandler(value = { CacheRefresherException.class })
-	public ResponseEntity<String> handleCacheRefresherException(RuntimeException e, WebRequest wr) {
+	public ResponseEntity<ResponseError> handleCacheRefresherException(RuntimeException e, WebRequest wr) {
 		String error = Optional.of(e.getMessage()).orElse(e.getClass().getName())
-				+ "\n[Cache refresher exception! => (CacheRefresherException)]";
-		logger.error(error, e);
-		return new ResponseEntity<>(error, HttpStatus.EXPECTATION_FAILED);
+				+ " [Cache refresher exception! => (CacheRefresherException)]";
+		ResponseError errorResponse = new ResponseError().createdAt(LocalDateTime.now().toString())
+				.detailedMessage(error).errorCode(HttpStatus.EXPECTATION_FAILED.value())
+				.exceptionName(CacheRefresherException.class.getName()).errorMessage(e.getMessage());
+		logger.error(errorResponse.toString(), e);
+		return new ResponseEntity<>(errorResponse, HttpStatus.EXPECTATION_FAILED);
 	}
 }
